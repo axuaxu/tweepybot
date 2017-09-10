dc2826e055087ffc99ca67276e1a7add2dbf27d3
axuaxu <axu0110@gmail.com>
2017-09-09 20:51:18 -0400

    1

---
+++ keys.py
@@ -0,0 +1,8 @@
+#https://dototot.com/write-twitter-bot-python-tweepy-follow-new-followers/
+#visit http://dev.twitter.com to create an application and get your keys
+keys = dict(
+    consumer_key =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    consumer_secret =       'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token_secret =   'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+)
